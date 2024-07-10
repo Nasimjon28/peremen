7252cfbf20e5978a2070b6eb85ddf97da0d6479b
Nasimjon28 <shahlo.reg@gmail.com>
2024-07-10 08:57:54 +0300

    fdbfd

---
+++ main.py
@@ -0,0 +1,5 @@
+dz_count = 12
+time_spent = 1.5
+name = "Python"
+Time_for_one_homework = dz_count / time_spent
+print(name,"всего задач",dz_count,"затрачено часов",time_spent,"среднее время выполнения",Time_for_one_homework)
